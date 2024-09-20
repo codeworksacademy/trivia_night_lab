c40e81c022cc66488c1cc32fc2f854e26f1d9db3
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-20 11:35:14 -0600

    cleaned house

--- app/App.js
+++ app/App.js
@@ -1,10 +1,8 @@
 import { router } from './router-config.js';
-import { HomeController } from './controllers/HomeController.js';
 const USE_ROUTER = false
 
 class App {
 
-  HomeController = new HomeController()
 
   constructor() {
     if (USE_ROUTER) {
