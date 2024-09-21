caade2cdee6109c6c329c58a2321961b970485bb
Jeremy-Fowler <jembyfowler@gmail.com>
2024-09-20 22:11:17 -0600

    wait no answers needs the check

--- app/models/Question.js
+++ app/models/Question.js
@@ -12,7 +12,7 @@ export class Question {
     this.answered = false
     this.answeredCorrectly = false
     this.correctAnswer = data.correct_answer
-    this.answers = [this.correctAnswer, ...data.incorrect_answers,]
+    this.answers = this.type == 'boolean' ? ['True', 'False'] : [this.correctAnswer, ...data.incorrect_answers,]
     if (this.type == 'multiple') {
       this.correctAnswer = this.correctAnswer.replaceAll("'", "\'")
       this.answers = this.answers.map(answer => answer.replaceAll("'", "\'"))
